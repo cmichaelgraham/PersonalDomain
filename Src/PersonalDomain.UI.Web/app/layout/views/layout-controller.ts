b58076af7219739990bcb2b454c6124630b827ef
James Chadwick <James.Chadwick@gmail.com>
2015-05-10 20:55:51 -0400

    Fixed binding on the title element, updated the layout controller to include some 'TODO' notes

--- Src/PersonalDomain.UI.Web/app/layout/views/layout-controller.ts
+++ Src/PersonalDomain.UI.Web/app/layout/views/layout-controller.ts
@@ -1,4 +1,7 @@
 ﻿module PersonalDomain.Application.Layout {
+    //$jchadwick- TODO: https://stackoverflow.com/questions/12506329/how-to-dynamically-change-header-based-on-angularjs-partial-view/17898250#17898250
+    //            Possible way to go about refactoring?
+
     export class LayoutController {
         constructor($scope, header) {
             $scope.Header = header;
