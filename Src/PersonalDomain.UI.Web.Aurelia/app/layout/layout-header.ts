8aeee05d878e3d9f8d1566f53a40785a148e5d0c
James Chadwick <James.Chadwick@gmail.com>
2015-06-27 13:01:15 -0400

    Revert "fixing stuff"
    This reverts commit a8482a41c0079690d3453c59efe693ce37774016.

--- Src/PersonalDomain.UI.Web.Aurelia/app/layout/layout-header.ts
+++
@@ -1,7 +0,0 @@
-﻿import aureliaFramework = require("aurelia-framework");
-
-class LayoutHeader {
-
-}
-
-export = LayoutHeader;
--- Src/PersonalDomain.UI.Web.Aurelia/app/layout/navigation-bar.ts
+++
@@ -1,7 +0,0 @@
-﻿import aureliaFramework = require("aurelia-framework");
-
-class NavigationBar {
-    static metadata = aureliaFramework.Behavior.withProperty("router");
-}
-
-export = NavigationBar;
