3f04f410925559ede499055acb614d627d513864
James P Chadwick <contact@jamespchadwick.com>
2015-09-30 21:55:28 -0400

    Hooking up delete post button

--- Src/PersonalDomain.UI.Web.Aurelia/src/blog/post/listing.ts
+++ Src/PersonalDomain.UI.Web.Aurelia/src/blog/post/listing.ts
@@ -15,6 +15,14 @@ export class PostListingViewModel {
 		});	
 	}
 	
+	public DeletePost(id: number): void {
+		this._blogDataService.DeletePostById({Id: id}).then((result) => {
+			if (result.IsSuccess) {
+				this.PostSummaries = this.PostSummaries.filter(p => p.Id != id);
+			}
+		});
+	}
+	
 	private LoadPostSummaries(): Promise<PersonalDomain.Application.Blogging.Models.PostSummaryDTO[]> {
 		return this._blogDataService.GetPostSummaries({});
 	}
