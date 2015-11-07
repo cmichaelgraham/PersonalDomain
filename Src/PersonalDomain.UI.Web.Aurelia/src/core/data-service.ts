f0caf6206aacb527d3896750b54910c6018f09c0
James P Chadwick <contact@jamespchadwick.com>
2015-11-06 20:28:05 -0500

    Taking out Authorization Infrastructure
    Lets see if bundling works with this stuff taken out...

--- Src/PersonalDomain.UI.Web.Aurelia/src/core/data-service.ts
+++ Src/PersonalDomain.UI.Web.Aurelia/src/core/data-service.ts
@@ -1,16 +1,16 @@
 import {HttpClient, RequestBuilder} from 'aurelia-http-client';
-import {AuthorizationInterceptor} from 'infrastructure/authorization-interceptor';
+// import {AuthorizationInterceptor} from 'infrastructure/authorization-interceptor';
 import {AuthenticationService} from 'account/services/authentication-service';
 
 export class CoreDataService {
-	constructor(public BaseUrl: string, public HttpClient: HttpClient, public AuthorizationInterceptor: AuthorizationInterceptor) {
+	constructor(public BaseUrl: string, public HttpClient: HttpClient) { //, public AuthorizationInterceptor: AuthorizationInterceptor
 	}
 	
 	public Post<TRequest, TResponse>(url: string, jsonRequest: TRequest): Promise<TResponse> {
 		var post = (<any>this.HttpClient).createRequest(this.BaseUrl + url)
 										 .asPost()
 										 .withHeader("Content-Type", "application/json;charset=UTF-8")
-										 .withInterceptor(this.AuthorizationInterceptor)
+										//  .withInterceptor(this.AuthorizationInterceptor)
 										 .withContent(jsonRequest);
 		
 		if (AuthenticationService.IsAuthenticated()) {
--- Src/PersonalDomain.UI.Web.Aurelia/src/infrastructure/authorization-pipeline-step.ts
+++ Src/PersonalDomain.UI.Web.Aurelia/src/infrastructure/authorization-pipeline-step.ts
@@ -1,12 +1,12 @@
-import {Redirect} from "aurelia-router";
-import {AuthenticationService} from "account/services/authentication-service";
-
-export class AuthorizationPipelineStep {
-	public run(routingContext, next) {
-		if (routingContext.nextInstructions.some(i => i.config.authorize)) {
-			return AuthenticationService.IsAuthenticated() ? next() : next.cancel(new Redirect('admin'));	
-		}
-		
-		return next();
-	}
-}
+// import {Redirect} from "aurelia-router";
+// import {AuthenticationService} from "account/services/authentication-service";
+// 
+// export class AuthorizationPipelineStep {
+// 	public run(routingContext, next) {
+// 		if (routingContext.nextInstructions.some(i => i.config.authorize)) {
+// 			return AuthenticationService.IsAuthenticated() ? next() : next.cancel(new Redirect('admin'));	
+// 		}
+// 		
+// 		return next();
+// 	}
+// }
